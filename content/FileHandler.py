0f80912b2ea640991f3fa7108674a14757ea0b20
Ronny Standtke <ronny.standtke@gmx.net>
2026-03-12 17:57:50 +0100

    fixed error handling in FileHandler

--- content/FileHandler.py
+++ content/FileHandler.py
@@ -2,7 +2,6 @@ from datetime import date
 from IPython.display import display, HTML, clear_output
 import base64
 import json
-import traceback
 
 
 class FileHandler:
@@ -18,57 +17,43 @@ class FileHandler:
         self.EMPLOYEES_KEY = "employees"
 
     def load_data(self, content):
-        try:
-            data = json.loads(bytes(content).decode('utf-8'))
-            return self._restore_dates(data)
-        except Exception:
-            print(traceback.format_exc())
-            with self.output:
-                print(traceback.format_exc())
+        return self._restore_dates(json.loads(bytes(content).decode('utf-8')))
 
     def save_data(self, year, annual_working_time, total_budget,
                   management_allowance, budgeted_sick_leave,
                   administration_percentage, df, download_output):
 
-        try:
+        data_to_export = {
+            self.YEAR_KEY: year,
+            self.ANNUAL_WORKING_TIME_KEY: annual_working_time,
+            self.TOTAL_BUDGET_KEY: total_budget,
+            self.MANAGEMENT_ALLOWANCE_KEY: management_allowance,
+            self.BUDGETED_SICK_LEAVE_KEY: budgeted_sick_leave,
+            self.ADMINISTRATION_PERCENTAGE_KEY: administration_percentage,
+            self.EMPLOYEES_KEY: df.to_dict(orient='records')
+        }
 
-            data_to_export = {
-                self.YEAR_KEY: year,
-                self.ANNUAL_WORKING_TIME_KEY: annual_working_time,
-                self.TOTAL_BUDGET_KEY: total_budget,
-                self.MANAGEMENT_ALLOWANCE_KEY: management_allowance,
-                self.BUDGETED_SICK_LEAVE_KEY: budgeted_sick_leave,
-                self.ADMINISTRATION_PERCENTAGE_KEY: administration_percentage,
-                self.EMPLOYEES_KEY: df.to_dict(orient='records')
-            }
+        json_str = json.dumps(
+            data_to_export,
+            indent=2,
+            default=self._json_serializer
+        )
 
-            json_str = json.dumps(
-                data_to_export,
-                indent=2,
-                default=self._json_serializer
-            )
+        b64 = base64.b64encode(json_str.encode()).decode()
 
-            b64 = base64.b64encode(json_str.encode()).decode()
-
-            html = f"""
-            <a id="download-link"
-            download="data.json"
-            href="data:text/json;base64,{b64}"
-            style="display:none;">
-            </a>
-            <script>
-                document.getElementById('download-link').click();
-            </script>
-            """
-
-            with download_output:
-                clear_output()
-                display(HTML(html))
-
-        except Exception:
-            print(traceback.format_exc())
-            with self.output:
-                print(traceback.format_exc())
+        html = f"""
+        <a id="download-link"
+        download="data.json"
+        href="data:text/json;base64,{b64}"
+        style="display:none;">
+        </a>
+        <script>
+            document.getElementById('download-link').click();
+        </script>
+        """
+        with download_output:
+            clear_output()
+            display(HTML(html))
 
     def _json_serializer(self, obj):
         if isinstance(obj, date):
